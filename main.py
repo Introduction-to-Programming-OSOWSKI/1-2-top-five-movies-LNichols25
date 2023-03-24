5e02894a78c004e7d89d2c386bf45be88b876908
LNichols25 <826699@mystma.org>
2023-03-24 15:05:06 -0500

    2

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-def TopFiveMovies(a, b, c, d, e):
-    return "a" + "b" + "c" + "d" + "e"
+def topFiveMovies(a, b, c, d, e):
+    return a + " " + b + " " + c + " " + d + " "+ e
+
+print(topFiveMovies("movie1", "movie2", "movie3", "movie4", "movie5"))
